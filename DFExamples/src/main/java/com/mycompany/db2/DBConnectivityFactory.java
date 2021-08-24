2c42986ed45b63b923d0d75992d3e167140dd1c8
dcdhananjay <37080640+dcdhananjay@users.noreply.github.com>
2021-08-24 12:48:21 +0530

    Delete DFExamples/src/main/java/com/mycompany/db2 directory

--- DFExamples/src/main/java/com/mycompany/db2/DBConnectivityFactory.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.db2;
-
-public class DBConnectivityFactory {
-
-    public static IDBConnectivity GetDataAccessObj() {
-        return new DBConnectivity();
-    }
-}
--- DFExamples/src/main/java/com/mycompany/db2/IDBConnectivity.java
+++
@@ -1,6 +0,0 @@
-package com.mycompany.db2;
-
-public interface IDBConnectivity {
-
-    void makeConnection(String url, String username, String password, String tablename);
-}
